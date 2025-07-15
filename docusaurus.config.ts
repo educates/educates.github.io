cc6675c11f3df791e73237ae02727d0ff7b6db1f
Jorge Morales Pou <jorgemoralespou@gmail.com>
2025-07-15 09:55:30 +0200

    Adding gtag

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -118,6 +118,10 @@ const config: Config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
+        gtag: {
+          trackingID: "G-6E8JHKBWNX",
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
